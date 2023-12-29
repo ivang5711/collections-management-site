be22d3e3a8d24e480c2e6f8caf9ecbeaad06be02
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2023-12-30 00:37:16 +0400

    Adds Item details page
    Added Item details page and linked
    it to the collections page elements.

--- Collections/Collections/Models/Collection.cs
+++ Collections/Collections/Models/Collection.cs
@@ -1,8 +1,9 @@
 ﻿namespace Collections.Models;
 
-public partial class Collection(List<Item> items, string name, string description, string theme, string? imageLink = null)
+public partial class Collection(int id, List<int> items, string name, string description, string theme, string? imageLink = null)
 {
-    public List<Item> Items { get; set; } = items;
+    public int Id { get; set; } = id;
+    public List<int> Items { get; set; } = items;
 
     public int TotalItems { get; } = items.Count;
 

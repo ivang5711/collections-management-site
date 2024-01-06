7f21268215d35b25373788b3d6529dc0d09d52c8
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2024-01-06 19:39:41 +0400

    Refactors Application user class

--- Collections/Collections/Data/ApplicationUser.cs
+++ Collections/Collections/Data/ApplicationUser.cs
@@ -2,12 +2,12 @@ using Microsoft.AspNetCore.Identity;
 
 namespace Collections.Data
 {
-    // Add profile data for application users by adding properties to the ApplicationUser class
+    // Add profile data for application users by adding properties to
+    // the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
         public DateTime LastLoginDate { get; set; }
 
         public DateTime RegistrationDate { get; set; }
     }
-
 }

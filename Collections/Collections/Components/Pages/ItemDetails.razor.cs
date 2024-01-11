5f22075e2b62a53e35516749bdda14439fb8f1a2
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2024-01-11 05:17:02 +0400

    Adds Like button to the item details page

--- Collections/Collections/Components/Pages/ItemDetails.razor.cs
+++ Collections/Collections/Components/Pages/ItemDetails.razor.cs
@@ -12,12 +12,19 @@ public partial class ItemDetails
     private Item? _itemDetails;
     private List<Item>? _itemsBunch;
     private List<Comment> _comments = [];
+    public int LikeCount { get; set; } = 999;
 
     protected override async Task OnInitializedAsync()
     {
         _itemsBunch = CreateData2();
         _itemDetails = _itemsBunch.First(x => x.Id == Id);
         _comments = _itemDetails.Comments;
+        LikeCount = _itemDetails.Likes.Count;
+    }
+
+    private void IncrementLike()
+    {
+        LikeCount++;
     }
 
     private List<Item> CreateData2()
@@ -26,6 +33,7 @@ public partial class ItemDetails
         .Include(e => e.Tags)
         .Include(e => e.Likes)
         .Include(e => e.Comments)
+        .Include (e => e.Collection)
         .ToList();
         return t;
     }

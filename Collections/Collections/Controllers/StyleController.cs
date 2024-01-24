379dd69583e2424183f988bc36572ca824c4a897
ivang5711 <122921327+ivang5711@users.noreply.github.com>
2024-01-24 14:54:41 +0400

    Adds day/night mode

---
+++ Collections/Collections/Controllers/StyleController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Collections.Controllers
+{
+    [Route("/[controller]")]
+    [ApiController]
+    public class StyleController : ControllerBase
+    {
+        public async Task<ActionResult> ChangeStyle([FromQuery] string style)
+        {
+            Response.Cookies.Append("style", style);
+            return Redirect("/");
+        }
+    }
+}
